6f6cc7c4a636ace9512d59a058d23e0487a755bc
Jon Castillo <jcastillo@evidon.com>
2016-08-22 11:17:24 -0400

    another edit

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-  config.account_sid = "AC618535f2a856c7b96e55c53c9a69b867"
-  config.auth_token = "17fbeecc9a6718e179cb654a77f841cf"
+  config.account_sid = ""
+  config.auth_token = ""
 end
